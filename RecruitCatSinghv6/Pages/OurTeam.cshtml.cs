c096da5fe75c239df6ae85df5dfca264607904b4
vs311 <vs311@MEANMACHINE>
2022-09-06 23:11:12 -0400

    Add project files.

---
+++ RecruitCatSinghv6/Pages/OurTeam.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RecruitCatSinghv6.Pages
+{
+    public class OurTeamModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
